56b4c0b9a280575fbb66e5cc12655b238cb9881a
Ritesh Kumar <riteshk8055@gmail.com>
2023-09-02 22:38:38 +0530

    js code started

---
+++ script.js
@@ -0,0 +1,5 @@
+const gameCells = document.querySelectorAll('.cell');
+const player1 = document.querySelector('.player1');
+const player2 = document.querySelector('.player2');
+const restartBtn = document.querySelector('.restartBtn');
+
