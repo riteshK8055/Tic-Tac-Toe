cf577a3feec22c62b9fcb890b53aa15ed7e6fe99
Ritesh Kumar <riteshk8055@gmail.com>
2023-09-03 21:58:37 +0530

    function to start game added

--- script.js
+++ script.js
@@ -3,3 +3,35 @@ const player1 = document.querySelector('.player1');
 const player2 = document.querySelector('.player2');
 const restartBtn = document.querySelector('.restartBtn');
 
+//making variables 
+
+let  currentPlayer = 'X';
+let nextPlayer = 'O';
+let playerTurn = currentPlayer;
+
+//Function to start your game
+const startGame = () =>{
+
+    gameCells.forEach(cell => {
+
+        cell.addEventListener('click',(e) =>{
+
+            if(e.target.textContent === ''){
+
+                e.target.textContent = playerTurn;
+                changePlayerTurn();
+            }
+           
+           
+           
+        });
+    });
+}
+
+const changePlayerTurn = () =>{
+
+    playerTurn = playerTurn === currentPlayer ? nextPlayer : currentPlayer;
+}
+
+startGame();
+
